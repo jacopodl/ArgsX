7998e020a70e0bf24e957e9d102abc98dfbc7187
Jacopo <jacopo.delu@gmail.com>
2015-04-20 21:41:01 +0200

    Project update

--- src/ArgsX.h
+++ src/ArgsX.h
@@ -1,125 +1,54 @@
 /*
-    <ArgsX is a simple c/c++ options parser>
-    Copyright (C) <2013>  <Jacopo De Luca>
+	* <ArgsX, The simple C/C++ options parser.>
+	* Copyright (C) <2014-2015> <Jacopo De Luca>
+	*
+	* This program is free software: you can redistribute it and/or modify
+	* it under the terms of the GNU General Public License as published by
+	* the Free Software Foundation, either version 3 of the License, or
+	* (at your option) any later version.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	* This program is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	* GNU General Public License for more details.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	* You should have received a copy of the GNU General Public License
+	* along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <string.h>
+#ifndef argsxh
+#define argsxh
 
-// Constant
-#define _ARGSX_BadArg 0xBED
-#define _ARGSX_LowArg 0x10A
-#define _ARGSX_OtherData 0xCDA
-#define _ARGSX_LongOption 0x10D
-#define _ARGSX_FINISH -1
-// -------------------------------------------
-#define _ARGSX_OPT_NO_ARGUMENTS 0x0
-#define _ARGSX_OPT_REQUIRED_ARGUMENT 0x1
-#define _ARGSX_OPT_REQUIRES_MORE_ARGUMENTS 0x2
+extern char *ax_curr; // Pointer to current position string
+extern char *ax_arg; // Pointer to arg
+extern int ax_cursor; // Current position
+extern int ax_opterr; // Show errors
+extern short ax_etype; // Error type
+extern unsigned short ax_loptidx; // Long options index
 
-struct _ArgsX_LongOpt
+/* Switch constant */
+#define ARGSX_BAD_OPT  0xBA
+#define ARGSX_FEW_ARGS  0xFA
+#define ARGSX_NONOPT  0xCE
+#define ARGSX_LOPT  0xAA
+
+/* ax_lopt mode */
+#define ARGSX_NOARG 0x00
+#define ARGSX_REQ_ARG 0x01
+
+typedef struct ax_lopt
 {
     char *name;
-    int Args;
+    int args;
     char opt;
-};
+}ax_lopt;
 
-int ArgsX_ArgPtr=-1,
-    ArgsX_LOpt_ptr=-1;
-
-int ArgsX(int Argc,char **Argv,int *arg_ptr,char *Opt,struct _ArgsX_LongOpt *Lopt,unsigned Lopt_size,char tr)
+typedef enum ax_einfo
 {
-    if(*arg_ptr>=Argc)
-        return _ARGSX_FINISH;
+	short_opt,
+	long_opt
+}ax_einfo;
 
-    if(*Argv[*arg_ptr]==tr)
-    {
-        if(*++Argv[*arg_ptr]==tr&&Lopt!=0)
-        {
-            Argv[*arg_ptr]++;
-            for(unsigned i=0;i<(Lopt_size/sizeof(_ArgsX_LongOpt));i++)
-            {
-                if(strcmp(Lopt[i].name,Argv[*arg_ptr])==0)
-                {
-                    int ret = ((int)Lopt[i].opt==0)?_ARGSX_LongOption:((int)Lopt[i].opt);
-                    ArgsX_LOpt_ptr=i;
-                    if(Lopt[i].Args==_ARGSX_OPT_NO_ARGUMENTS)
-                    {
-                        return ret;
-                    }
-                    else if(++*arg_ptr<Argc&&*Argv[*arg_ptr]!=tr)
-                    {
-                        if(Lopt[i].Args==_ARGSX_OPT_REQUIRED_ARGUMENT)
-                        {
-                            ArgsX_ArgPtr=*arg_ptr;
-                            return ret;
-                        }
-                        else if(Lopt[i].Args==_ARGSX_OPT_REQUIRES_MORE_ARGUMENTS)
-                        {
-                            for(int j=*arg_ptr;j<Argc;j++)
-                            {
-                                if(*Argv[j]!=tr)
-                                    ArgsX_ArgPtr=j;
-                                else
-                                    break;
-                            }
-                            return ret;
-                        }
-                    }
-                    else
-                        return _ARGSX_LowArg;
-                }
-            }
-            return _ARGSX_BadArg;
-        }
-        else
-        {
-            char *subarg,
-                  RetChar=*Argv[*arg_ptr];
-
-            if((subarg=strstr(Opt,Argv[*arg_ptr]))!=0)
-            {
-                if(*++subarg=='+')
-                {
-                    if(++*arg_ptr<Argc&&*Argv[*arg_ptr]!=tr)
-                    {
-                        if(*++subarg=='+')
-                        {
-                            for(int i=*arg_ptr;i<Argc;i++)
-                            {
-                                if(*Argv[i]!=tr)
-                                    ArgsX_ArgPtr=i;
-                                else
-                                    break;
-                            }
-                        }
-                        else
-                            ArgsX_ArgPtr=*arg_ptr;
-                    }
-                    else
-                        return _ARGSX_LowArg;
-                }
-            }
-            else
-                return _ARGSX_BadArg;
-
-            return (int)RetChar;
-        }
-    }
-    ArgsX_ArgPtr=*arg_ptr;
-    return _ARGSX_OtherData;
-}
-
-
+/* prototype */
+int argsx(int argc, char **argv, char *opt, ax_lopt *lopt, unsigned short lopt_size,char tr);
+#endif
